eba3f99d8d03d027ad7f1e3025d033ea5736f559
Lili Boxer <ninaliboxer@gmail.com>
2019-10-02 10:48:00 -0700

    GET and PATCH route created and tested

--- test/items.test.js
+++ test/items.test.js
@@ -5,6 +5,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
+const Item = require('../lib/models/Item');
+
 describe('app routes', () => {
   beforeAll(() => {
     connect();
@@ -21,17 +23,55 @@ describe('app routes', () => {
   it('creates an item', () => {
     return request(app)
       .post('/api/v1/items')
-      .send({
-        category: 'clothing',
-        name: 't-shirt',
-        count: 10
+      .send({ 
+        category: 'clothing', 
+        name: 't-shirt', 
+        count: 10 
       })
       .then(res => {
         expect(res.body).toEqual({
+          _id: expect.any(String), 
+          category: 'clothing', 
+          name: 't-shirt', 
+          count: 10 });
+      });
+  });
+
+  it('gets all items', async() => {
+    const items = JSON.parse(JSON.stringify(await Item.create([
+      { category: 'outdoor', name: 'hiking boots', count: 2 },
+      { category: 'kitchen', name: 'skillet', count: 1 },
+      { category: 'clothing', name: 'dress pants', count: 4 },
+    ])));
+    return request(app)
+      .get('/api/v1/items')
+      .then(res => {
+        items.forEach(item => {
+          expect(res.body).toContainEqual({
+            _id: item._id.toString(),
+            category: item.category,
+            name: item.name,
+            count: item.count
+          });
+        });
+      });
+  });
+
+  it('update count of item', async() => {
+    const item = await Item.create({ 
+      category: 'bathroom', 
+      name: 'soap', 
+      count: 10 
+    });
+    return request(app)
+      .patch(`/api/v1/items/${item._id}`)
+      .send({ count: 3 })
+      .then(res => {
+        expect(res.body).toEqual({
           _id: expect.any(String),
-          category: 'clothing',
-          name: 't-shirt',
-          count: 10
+          category: 'bathroom',
+          name: 'soap',
+          count: 3
         });
       });
   });
